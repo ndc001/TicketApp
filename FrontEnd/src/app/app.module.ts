0438ab7f27bb86209bd58f52d3449ac9b1c770aa
ndc001 <66589760+ndc001@users.noreply.github.com>
2024-05-24 12:34:38 -0400

    Adding some components to FrontEnd. Ticket, Login, Register

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,13 +3,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TicketsComponent } from './tickets/tickets.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketsComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(
+      appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
